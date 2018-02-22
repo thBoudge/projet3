db0c777bb1b9da7dd6e050c96386db2307c56437
thBoudge <thomas.bouges@gmail.com>
2018-02-22 15:33:38 -0500

    création class fille Epee.swift, Hache.swift, soins.swift, Marteau.swift de Weapon + var + init

---
+++ Warriors/Warriors/Epee.swift
@@ -0,0 +1,22 @@
+//
+//  Epee.swift
+//  Warriors
+//
+//  Created by thomas bouges on 18-02-22.
+//  Copyright © 2018 thomasbouges. All rights reserved.
+//
+
+import Foundation
+
+class Epee: Weapon{
+    
+    
+    init() {
+        
+        super.init(nameWeapon: "Epee", powerWeapon: 10)
+        
+    }
+    
+    
+}
+
