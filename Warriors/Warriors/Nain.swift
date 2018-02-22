47a0de2a51f31aa7f1099869ec342fc43c9e1b02
thBoudge <thomas.bouges@gmail.com>
2018-02-22 15:17:40 -0500

    création class fille Nain.swift de Combattant + var + init

---
+++ Warriors/Warriors/Nain.swift
@@ -0,0 +1,24 @@
+//
+//  Nain.swift
+//  Warriors
+//
+//  Created by thomas bouges on 18-02-22.
+//  Copyright © 2018 thomasbouges. All rights reserved.
+//
+
+import Foundation
+
+class Nain: Warrior{
+    
+    var typeAttac: String
+    
+    init(name: String) {
+        
+        self.typeAttac = "strike"
+        super.init(name: name,power: 120)
+        
+    }
+    
+    
+}
+
