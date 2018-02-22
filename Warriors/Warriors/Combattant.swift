e6f171d3c289c8fb2713f2af2a01b7f25c9261ff
thBoudge <thomas.bouges@gmail.com>
2018-02-22 15:05:49 -0500

    création class fille Combattant.swift + var + init

---
+++ Warriors/Warriors/Combattant.swift
@@ -0,0 +1,25 @@
+//
+//  Combattant.swift
+//  Warriors
+//
+//  Created by thomas bouges on 18-02-22.
+//  Copyright © 2018 thomasbouges. All rights reserved.
+//
+
+import Foundation
+
+class Combattant: Warrior{
+    
+    var typeAttac: String
+    
+    init(name: String) {
+        
+        self.typeAttac = "strike"
+        super.init(name: name,power: 100)
+        
+    }
+    
+    
+}
+
+
