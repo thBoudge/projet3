7de2bedfbe5823150361d4cd0b132e24303ec482
thBoudge <thomas.bouges@gmail.com>
2018-03-07 17:00:51 -0500

    New Weapon Thunder.swift

---
+++ Warriors/Warriors/Weapon/Thunder.swift
@@ -0,0 +1,21 @@
+//
+//  Thunder.swift
+//  Warriors
+//
+//  Created by thomas bouges on 18-03-07.
+//  Copyright © 2018 thomasbouges. All rights reserved.
+//
+
+import Foundation
+
+class Thunder: Weapon{
+    
+    
+    init() {
+        
+        super.init(nameWeapon: "Thunder", powerWeapon: 100)
+        
+    }
+    
+    
+}
