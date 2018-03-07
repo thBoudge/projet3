122cdef3052ce695e773c54aa1615308589f8352
thBoudge <thomas.bouges@gmail.com>
2018-03-07 16:54:12 -0500

    New Weapon Wine.swift

---
+++ Warriors/Warriors/Weapon/Wine.swift
@@ -0,0 +1,21 @@
+//
+//  Wine.swift
+//  Warriors
+//
+//  Created by thomas bouges on 18-03-07.
+//  Copyright © 2018 thomasbouges. All rights reserved.
+//
+
+import Foundation
+
+class Wine: Weapon{
+    
+    
+    init() {
+        
+        super.init(nameWeapon: "Wine", powerWeapon: 50)
+        
+    }
+    
+    
+}
