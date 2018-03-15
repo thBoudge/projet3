f691ad83c6656e075c94ad04b79245a12edf687b
thBoudge <thomas.bouges@gmail.com>
2018-03-15 10:54:57 -0400

    class Input.swift creation + func static InputInt() et inputString() + modification de tous le code pour integrer new func

---
+++ Warriors/Warriors/Input.swift
@@ -0,0 +1,27 @@
+//
+//  Input.swift
+//  Warriors
+//
+//  Created by thomas bouges on 18-03-15.
+//  Copyright © 2018 thomasbouges. All rights reserved.
+//
+
+import Foundation
+
+class Input {
+
+    /////////////////////////////////////////Methods to get player reponse and return an int ////////////////////////////
+    
+    static func inputInt() -> Int {
+        guard let data = readLine() else { return 0 }
+        guard let dataToInt = Int(data) else { return 0 }
+        return dataToInt
+    }
+    
+    static func inputString() -> String {
+        guard let data = readLine() else { return "" }
+        return data
+    }
+
+
+}
