434072a07a251b362d2ab45845128dbc44271120
thBoudge <thomas.bouges@gmail.com>
2018-03-21 09:13:58 -0400

    Creation Class NameService + func uniqueTeamName() & warriorName() + modification code

---
+++ Warriors/Warriors/NameService.swift
@@ -0,0 +1,59 @@
+//
+//  NameService.swift
+//  Warriors
+//
+//  Created by thomas bouges on 18-03-16.
+//  Copyright © 2018 thomasbouges. All rights reserved.
+//
+
+import Foundation
+
+class NameService{
+    
+    var teamNames = [String]()
+    var warriorNames = [String]()
+    
+    //Pattern Singleton static let et private init
+    static let shared = NameService()
+    private init(){}
+    
+    // Method tto check if there is not an egual team name
+    func uniqueTeamName() -> String {
+        
+        var teamName = ""
+        repeat {
+            print("Name of Your Team :  ")
+            teamName = Input.inputString()
+            if teamNames.contains(teamName){
+                
+                print("this team name already exist")
+                teamName = ""
+            }
+            teamNames.append(teamName)
+            
+        }while teamName == ""
+        return teamName
+    }
+    
+    func warriorname () -> String{
+        var nameWarrior = ""
+        
+        repeat{
+            print("Nom du Warrior :   ")
+            nameWarrior = Input.inputString()
+            if warriorNames.contains(nameWarrior){
+                
+                print("this warrior name already exist")
+                nameWarrior = ""
+            }
+            warriorNames.append(nameWarrior)
+        }while  nameWarrior == ""
+        return nameWarrior
+    }
+    
+    
+    
+}
+
+
+
